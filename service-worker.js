d3af9d3a1f7c6e2b406ed9598c9716e073e17496
ojwinservice-web <ojwinservice@gmail.com>
2026-07-04 10:34:03 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ojwin-crm-v1';
+const CACHE_NAME = 'ojwin-crm-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
