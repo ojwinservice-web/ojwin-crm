31223993021158d1420d1406e932fc8753fc97e8
Farzad <farzad@example.com>
2026-08-08 18:58:56 +0330

    bump sw cache v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ojwin-crm-v11';
+const CACHE_NAME = 'ojwin-crm-v12';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './cloud.js'];
 
 self.addEventListener('install', (event) => {
